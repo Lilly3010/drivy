6409e6763ec996f373fdb56ccf400ca08bc0469e
Lilly3010 <aurelie.le_calvez@devinci.fr>
2017-01-17 20:56:34 +0100

    Finish -all exercises

--- public/index.js
+++ public/index.js
@@ -167,44 +167,52 @@ var rentalModifications = [{
 
 //EXERCISE 1
 
-function setPrice_and_Commission(cars, rentals){
-	for(var i=0; i<rentals.length;++i){
-		var carOfRental = getCarById(cars, rentals[i].carId);
-		var PickDate =  new Date(rentals[i].pickupDate);
-		var ReturnDate = new Date(rentals[i].returnDate);
-		var nbDays = Math.ceil((ReturnDate - PickDate)/(1000*60*60*24)) + 1;
-		
-		var pricePerDay = carOfRental.pricePerDay;
-		
-		//exercise 2 -- start
-		if(nbDays > 10){
-			pricePerDay *= 0.5;
-		} else if(nbDays >4){
-			pricePerDay *= 0.7;	
-		}else if(nbDays>1){
-			pricePerDay *= 0.9;
+function setPrice_and_Commission(cars, rentals, rentalId = 0){
+	if(rentalId == 0){
+		for(var i=0; i<rentals.length;++i){
+			setPriceRental(cars, rentals, rentals[i]);
 		}
-		//exercise 2 --end
-		
-		var Time = nbDays * pricePerDay;
-		var Distance = rentals[i].distance * carOfRental.pricePerKm;
-		
-		rentals[i].price = Time + Distance;
-		
-		//exercise 3 --start
-		var commission = rentals[i].price*0.3;
-		rentals[i].commission.assistance = nbDays;
-		rentals[i].commission.insurance = commission*0.5;
-		rentals[i].commission.drivy = commission - rentals[i].commission.assistance - rentals[i].commission.insurance;
-		//exercise 3 --end
-		
-		//exercise 4 --start
-		if(rentals[i].options.deductibleReduction){
-			var AmountDeductible = (nbDays*4);
-			rentals[i].price += AmountDeductible;
-		}
-		//exercise 4 --end
+	} else {
+		setPriceRental(cars, rentals, rentalId);
 	}
+}
+
+function setPriceRental(cars, rentals, rentalId){
+	var carOfRental = getCarById(cars, rentalId.carId);
+	var PickDate =  new Date(rentalId.pickupDate);
+	var ReturnDate = new Date(rentalId.returnDate);
+	var nbDays = Math.ceil((ReturnDate - PickDate)/(1000*60*60*24)) + 1;
+	
+	var pricePerDay = carOfRental.pricePerDay;
+	
+	//exercise 2 -- start
+	if(nbDays > 10){
+		pricePerDay *= 0.5;
+	} else if(nbDays >4){
+		pricePerDay *= 0.7;	
+	}else if(nbDays>1){
+		pricePerDay *= 0.9;
+	}
+	//exercise 2 --end
+	
+	var Time = nbDays * pricePerDay;
+	var Distance = rentalId.distance * carOfRental.pricePerKm;
+	
+	rentalId.price = Time + Distance;
+	
+	//exercise 3 --start
+	var commission = rentalId.price*0.3;
+	rentalId.commission.assistance = nbDays;
+	rentalId.commission.insurance = commission*0.5;
+	rentalId.commission.drivy = commission - rentalId.commission.assistance - rentalId.commission.insurance;
+	//exercise 3 --end
+	
+	//exercise 4 --start
+	if(rentalId.options.deductibleReduction){
+		var AmountDeductible = (nbDays*4);
+		rentalId.price += AmountDeductible;
+	}
+	//exercise 4 --end
 }
 
 function getCarById(cars, Id){
@@ -225,64 +233,111 @@ function getRentalById(rentals, Id){
 	return;
 }
 
-function PayActors(actors, rentals){
-	for(var i=0; i<actors.length;++i){
-		var ActualRental = getRentalById(rentals,actors[i].rentalId);
-		
-		var PickDate =  new Date(ActualRental.pickupDate);
-		var ReturnDate = new Date(ActualRental.returnDate);
-		var nbDays = Math.ceil((ReturnDate - PickDate)/(1000*60*60*24)) + 1;
-		
-		//Let's get the amount for each actor
-		var PriceDriver = ActualRental.price;
-		var PriceInsurance = ActualRental.commission.insurance;
-		var PriceAssistance = ActualRental.commission.assistance;
-		var PriceDrivy = ActualRental.commission.drivy;
-		//Don't forget the deductive thing
-		if(rentals[i].options.deductibleReduction){
-			var AmountDeductible = (nbDays*4);
-			PriceDrivy += AmountDeductible;
+function getActorsById(actors, Id){
+	for( var a in actors){
+		if(actors[a].rentalId == Id){
+			return actors[a];
 		}
-		
-		var PriceOwner = PriceDriver - (PriceAssistance+PriceDrivy+PriceInsurance);
-		
-		//Set up the amount
-		for(var i=0; i<actors[i].payment.length;++i){
-			switch(actors[i].payment.who){
-				case "driver" :
-					actors[i].payment.amount = PriceDriver;
-				break;
-				
-				case "owner" :
-					actors[i].payment.amount = PriceOwner;
-				break;
-				
-				case "insurance" :
-					actors[i].payment.amount = PriceInsurance;
-				break;
-				
-				case "assistance" :
-					actors[i].payment.amount = PriceAssistance;
-				break;
-				
-				case "drivy" :
-					actors[i].payment.amount = PriceDrivy;					
-				break;
-			}
+	}
+	return;
+}
+
+//Exercise 5
+
+function PayActors(actors, rentals, actorsId = 0){
+	if(actorsId == 0){
+		for(var i=0; i<actors.length;++i){
+			PayActorsRental(actors, rentals, actors[i])
 		}
-		
-		
+	} else {
+		PayActorsRental(actors, rentals, actorsId);
+	}
+	
+}
+
+function PayActorsRental(actors, rentals, actorsId){
+	
+	var ActualRental = getRentalById(rentals, actorsId.rentalId);
+	
+	var PickDate =  new Date(ActualRental.pickupDate);
+	var ReturnDate = new Date(ActualRental.returnDate);
+	var nbDays = Math.ceil((ReturnDate - PickDate)/(1000*60*60*24)) + 1;
+	
+	//Let's get the amount for each actor
+	var PriceDriver = ActualRental.price;
+	var PriceInsurance = ActualRental.commission.insurance;
+	var PriceAssistance = ActualRental.commission.assistance;
+	var PriceDrivy = ActualRental.commission.drivy;
+	//Don't forget the deductive thing
+	if(ActualRental.options.deductibleReduction){
+		var AmountDeductible = (nbDays*4);
+		PriceDrivy += AmountDeductible;
+	}
+	
+	var PriceOwner = PriceDriver - (PriceAssistance+PriceDrivy+PriceInsurance);
+	
+	//Set up the amount
+	for(var i=0; i<actorsId.payment.length;++i){
+		switch(actorsId.payment[i].who){
+			case "driver" :
+				actorsId.payment[i].amount = PriceDriver;
+			break;
+			
+			case "owner" :
+				actorsId.payment[i].amount = PriceOwner;
+			break;
+			
+			case "insurance" :
+				actorsId.payment[i].amount = PriceInsurance;
+			break;
+			
+			case "assistance" :
+				actorsId.payment[i].amount = PriceAssistance;
+			break;
+			
+			case "drivy" :
+				actorsId.payment[i].amount = PriceDrivy;					
+			break;
+		}
 	}
 }
+
+//End exercise 5
+
+//Exercise 6
+function ModifyAndUpdate(rentals, rentalModifications,actors){
+	for(var i=0; i<rentalModifications.length;++i){
+		var ActualRental = getRentalById(rentals,rentalModifications[i].rentalId);
+		var ActualActors = getActorsById(actors,rentalModifications[i].rentalId);
+		/*var Distance = ActualRental.Distance;
+		var PickUp = ActualRental.pickupDate;
+		var Return = ActualRental.returnDate;*/
+		
+		if(rentalModifications[i].pickupDate != null){
+			ActualRental.pickupDate = rentalModifications[i].pickupDate;
+		}
+		
+		if(rentalModifications[i].returnDate != null){
+			ActualRental.returnDate = rentalModifications[i].returnDate;
+		}
+		
+		if(rentalModifications[i].distance != null){
+			ActualRental.distance = rentalModifications[i].distance;
+		}
+		
+		setPrice_and_Commission(cars, rentals, ActualRental)
+		PayActors(actors, rentals, ActualActors);
+	}
+}
+//End exercise 6
+
+
+
+setPrice_and_Commission(cars,rentals);
+PayActors(actors, rentals);
+ModifyAndUpdate(rentals, rentalModifications,actors);
 
 console.log(cars);
 console.log(rentals);
 console.log(actors);
-
-setPrice_and_Commission(cars,rentals);
-PayActors(actors, rentals);
-console.log(rentals);
-console.log(actors);
-
 console.log(rentalModifications);
-
